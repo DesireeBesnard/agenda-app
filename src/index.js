ad110c28f9ee621348366306d7931461666daf53
DesireeBesnard <desiree.besnard@gmail.com>
2021-01-20 19:24:20 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol lg="9">
+              <Event/>
+              <Event/>
+            </MDBCol>
+            <MDBCol lg="3"/>
+          </MDBRow>
+        </MDBContainer>
+      </React.Fragment>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Event extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h3>9:00 - Title</h3>
+      </React.Fragment>
+    )
+  }
+}
+ 
+ReactDOM.render(<App />, document.getElementById("root"));
