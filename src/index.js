d6714ae125f4a34332cd613cece71cfb5700ba9c
DesireeBesnard <desiree.besnard@gmail.com>
2021-01-25 19:19:47 +0100

    event element created

--- src/index.js
+++ src/index.js
@@ -7,15 +7,56 @@ import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import "./index.css";
 
 class App extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [
+        { id: 1,
+          time: "10:00", 
+          title: "Breakfast with Simon", 
+          location: "Lounge Caffe", 
+          description: "Discuss Q3 targets"
+        },
+
+        { id: 2,
+          time: "10:30", 
+          title: "Daily Standup Meeting (recurring)",
+          location: "Warsaw Spire Office", 
+          description: ""
+        },
+
+        { id: 3,
+          time: "11:00", 
+          title: "Call with HRs",
+        },
+
+        {
+          id: 4, 
+          time: "11:00",
+          title: "Lunch with Timothy",
+          location: "Canteen",
+          description: "Project evaluation ile declaring a variable and using an if statement is a fine way to conditionnally render a component, sometimes you might to use a"
+        }
+      ]
+    };
+  }
+  
   render() {
     return (
       <React.Fragment>
         <MDBContainer>
           <MDBRow>
             <MDBCol lg="9">
-              <Event/>
-              <Event/>
+              {this.state.events.map( event => (
+                <Event 
+                  key={event.id}
+                  id={event.id}
+                  time={event.time}
+                  title={event.title}
+                  location={event.location}
+                  description={event.description}
+                />
+            ))}
             </MDBCol>
             <MDBCol lg="3"/>
           </MDBRow>
@@ -26,13 +67,14 @@ class App extends Component {
 }
 
 class Event extends Component {
-  state = {};
   render() {
     return (
       <React.Fragment>
-        <h3>9:00 - Title</h3>
+        <h3>{this.props.time} - {this.props.title}</h3>
+        <h6>Location: {this.props.location}</h6>
+        <p>Desc: {this.props.description}</p>
       </React.Fragment>
-    )
+    );
   }
 }
  
